b2a85f7491c5327d99f870f180e0c04598625d76
servo pcerda <fernandonando98@live.com.mx>
2022-10-27 20:04:33 -0500

    login a medias

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -23,4 +23,49 @@ const register = async (req, res) => {
   }
 };
 
-export { register };
+//email y password  -----> TOKEN
+const login = async (req, res) => {
+  /**
+   * 1.- verificar que venga email y password ✅
+   * 2.- Buscar en base de datos si existe ese usuario con ese email ✅
+   *    Si no existe -> 401
+   * 3.- Verificamos que el password sea correcto bcrypt.compare ✅
+   *    Si no es correcta la contraseña -> 403
+   * 4.- Crear token y mandarlo
+   */
+
+  /**
+   * A      codificar  B
+   * payload  -> 🔑   token
+   * 97291u2bd12    -> 18273u912easkdujhaskjd
+   * token    -> 🔑   payload
+   */
+
+  const { body } = req;
+  if (!body.password || !body.email) {
+    return res.status(400).json({
+      msg: 'Ingresa correo y contraseña',
+    });
+  }
+  try {
+    const user = await User.findOne({
+      email: body.email,
+    });
+
+    if (!user) {
+      return res.status(403).json({
+        msg: 'Credenciales inválidas',
+      });
+    }
+
+    const isValid = await bcrypt.compare(body.password, user.password);
+
+    if (!isValid) {
+      return res.status(403).json({
+        msg: 'Credenciales inválidas',
+      });
+    }
+  } catch (error) {}
+};
+
+export { register, login };
